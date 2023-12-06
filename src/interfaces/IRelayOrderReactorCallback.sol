a8815e6ee75a066b7b68d30fde0ca97c0a070868
Eric Zhong <eric.zhong@uniswap.org>
2023-12-06 16:50:36 -0500

    natspec

--- src/interfaces/IRelayOrderReactorCallback.sol
+++ src/interfaces/IRelayOrderReactorCallback.sol
@@ -5,9 +5,8 @@ import {ResolvedRelayOrder} from "../base/ReactorStructs.sol";
 
 /// @notice Callback for executing orders through a reactor.
 interface IRelayOrderReactorCallback {
-    /// @notice Called by the reactor during the execution of an order
+    /// @notice Called by the reactor after actions are exected in an order
     /// @param resolvedOrders Has inputs and outputs
     /// @param callbackData The callbackData specified for an order execution
-    /// @dev Must have approved each token and amount in outputs to the msg.sender
     function reactorCallback(ResolvedRelayOrder[] memory resolvedOrders, bytes memory callbackData) external;
 }
