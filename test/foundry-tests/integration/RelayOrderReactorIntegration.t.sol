5db9fbbae61023555bf3a4d606eaab1b0844caeb
Eric Zhong <eric.zhong@uniswap.org>
2023-12-06 11:10:35 -0500

    remove constructor param

--- test/foundry-tests/integration/RelayOrderReactorIntegration.t.sol
+++ test/foundry-tests/integration/RelayOrderReactorIntegration.t.sol
@@ -64,7 +64,7 @@ contract RelayOrderReactorIntegrationTest is GasSnapshot, Test, Interop, PermitS
         json = vm.readFile(string.concat(root, "/test/foundry-tests/interop.json"));
         vm.createSelectFork(vm.envString("FOUNDRY_RPC_URL"), 17972788);
 
-        deployCodeTo("RelayOrderReactor.sol", abi.encode(PERMIT2, UNIVERSAL_ROUTER), RELAY_ORDER_REACTOR);
+        deployCodeTo("RelayOrderReactor.sol", abi.encode(PERMIT2), RELAY_ORDER_REACTOR);
         reactor = RelayOrderReactor(RELAY_ORDER_REACTOR);
         permitExecutor = new PermitExecutor(address(filler), reactor, address(filler));
 
