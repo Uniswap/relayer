8d0fc8aaea74225ff5d1832f1aba4e1522997a55
Eric Zhong <eric.zhong@uniswap.org>
2024-01-19 17:13:23 -0500

    Restrict to UR and don't encode actions (#37)
    * Restrict to UR and don't encode actions
    
    * forge fmt
    
    * Only pass in actions type

--- src/lib/ResolvedRelayOrderLib.sol
+++ src/lib/ResolvedRelayOrderLib.sol
@@ -11,22 +11,4 @@ library ResolvedRelayOrderLib {
             order.permit, order.details, order.swapper, order.hash, RelayOrderLib.PERMIT2_ORDER_TYPE, order.sig
         );
     }
-
-    function executeActions(ResolvedRelayOrder memory order) internal {
-        // actions are encoded as (address target, uint256 value, bytes data)[]
-        uint256 actionsLength = order.actions.length;
-        for (uint256 i = 0; i < actionsLength;) {
-            (address target, uint256 value, bytes memory data) = abi.decode(order.actions[i], (address, uint256, bytes));
-            (bool success, bytes memory result) = target.call{value: value}(data);
-            if (!success) {
-                // bubble up all errors, including custom errors which are encoded like functions
-                assembly {
-                    revert(add(result, 0x20), mload(result))
-                }
-            }
-            unchecked {
-                i++;
-            }
-        }
-    }
 }
