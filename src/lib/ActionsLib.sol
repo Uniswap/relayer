70cef1326c0392b58c23cec5e6bf28cc9e58ee5b
Sara Reynolds <snreynolds2506@gmail.com>
2024-02-08 15:18:08 -0500

    update compiler version

--- src/lib/ActionsLib.sol
+++ src/lib/ActionsLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.0;
 
 /// @notice Handles all calls to the universal router.
 library ActionsLib {
--- src/lib/RelayOrderLib.sol
+++ src/lib/RelayOrderLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.0;
 
 import {IPermit2} from "permit2/src/interfaces/IPermit2.sol";
 import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
