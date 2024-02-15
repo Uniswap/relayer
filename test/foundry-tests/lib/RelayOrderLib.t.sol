9ae8394e9d9d45ee6957612c76feb616c490a756
Sara Reynolds <snreynolds2506@gmail.com>
2024-02-15 11:56:56 -0500

    failing fuzz

--- test/foundry-tests/lib/RelayOrderLib.t.sol
+++ test/foundry-tests/lib/RelayOrderLib.t.sol
@@ -6,15 +6,18 @@ import {RelayOrderLib} from "../../../src/lib/RelayOrderLib.sol";
 import {IRelayOrderReactor} from "../../../src/interfaces/IRelayOrderReactor.sol";
 import {MockERC20} from "UniswapX/test/util/mock/MockERC20.sol";
 import {MockReactor} from "../util/mock/MockReactor.sol";
-import {RelayOrder, OrderInfo, FeeEscalator} from "../../../src/base/ReactorStructs.sol";
+import {RelayOrder, OrderInfo, FeeEscalator, Input} from "../../../src/base/ReactorStructs.sol";
 import {RelayOrderBuilder} from "../util/RelayOrderBuilder.sol";
 import {ReactorErrors} from "../../../src/base/ReactorErrors.sol";
 import {OrderInfoBuilder} from "../util/OrderInfoBuilder.sol";
 import {FeeEscalatorBuilder} from "../util/FeeEscalatorBuilder.sol";
+import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
+import {InputBuilder} from "../util/InputBuilder.sol";
 
 contract RelayOrderLibTest is Test {
     using OrderInfoBuilder for OrderInfo;
     using FeeEscalatorBuilder for FeeEscalator;
+    using InputBuilder for Input;
 
     address swapper;
     MockReactor reactor;
@@ -45,5 +48,70 @@ contract RelayOrderLibTest is Test {
         order.info.reactor = IRelayOrderReactor(address(0));
         vm.expectRevert(ReactorErrors.InvalidReactor.selector);
         reactor.validate(order);
+    }
+
+    function test_toTokenPermissions_default() public {
+        RelayOrder memory order = RelayOrderBuilder.initDefault(token, address(reactor), swapper);
+        ISignatureTransfer.TokenPermissions[] memory permissions = RelayOrderLib.toTokenPermissions(order);
+        assertEq(permissions.length, 2);
+        assertEq(permissions[0].token, address(token));
+        assertEq(permissions[1].token, address(token));
+        assertEq(permissions[0].amount, order.input.amount);
+        assertEq(permissions[1].amount, order.fee.endAmount);
+    }
+
+    function test_fuzz_toTokenPermissions(uint256 inputAmount, uint256 startAmount, uint256 endAmount) public {
+        RelayOrder memory order = RelayOrderBuilder.initDefault(token, address(reactor), swapper);
+        order.input = order.input.withAmount(inputAmount);
+        order.fee = order.fee.withStartAmount(startAmount).withEndAmount(endAmount);
+        ISignatureTransfer.TokenPermissions[] memory permissions = RelayOrderLib.toTokenPermissions(order);
+        assertEq(permissions.length, 2);
+        assertEq(permissions[0].token, address(token));
+        assertEq(permissions[1].token, address(token));
+        assertEq(permissions[0].amount, order.input.amount);
+        assertEq(permissions[1].amount, order.fee.endAmount);
+    }
+
+    function test_toTransferDetails_noEscalation() public {
+        RelayOrder memory order = RelayOrderBuilder.initDefault(token, address(reactor), swapper);
+        ISignatureTransfer.SignatureTransferDetails[] memory details =
+            RelayOrderLib.toTransferDetails(order, address(this));
+        assertEq(details.length, 2);
+        assertEq(details[0].to, address(0));
+        assertEq(details[0].requestedAmount, order.input.amount);
+        assertEq(details[1].to, address(this));
+        assertEq(details[1].requestedAmount, order.fee.startAmount);
+    }
+
+    function test_fuzz_toTransferDetails_noEscalation(uint256 startAmount, uint256 endAmount) public {
+        RelayOrder memory order = RelayOrderBuilder.initDefault(token, address(reactor), swapper);
+        order.fee = order.fee.withStartAmount(startAmount).withEndAmount(endAmount);
+        if (startAmount > endAmount) {
+            vm.expectRevert(ReactorErrors.InvalidAmounts.selector);
+        }
+        ISignatureTransfer.SignatureTransferDetails[] memory details =
+            RelayOrderLib.toTransferDetails(order, address(this));
+        assertEq(details.length, 2);
+        assertEq(details[0].to, address(0));
+        assertEq(details[0].requestedAmount, order.input.amount);
+        assertEq(details[1].to, address(this));
+        assertEq(details[1].requestedAmount, order.fee.startAmount);
+    }
+
+    function test_toTransferDetails_noEscalation_try() public {
+        uint256 startAmount = 0;
+        uint256 endAmount = 1;
+        RelayOrder memory order = RelayOrderBuilder.initDefault(token, address(reactor), swapper);
+        order.fee = order.fee.withStartAmount(startAmount).withEndAmount(endAmount);
+        if (startAmount > endAmount) {
+            vm.expectRevert(ReactorErrors.InvalidAmounts.selector);
+        }
+        ISignatureTransfer.SignatureTransferDetails[] memory details =
+            RelayOrderLib.toTransferDetails(order, address(this));
+        assertEq(details.length, 2);
+        assertEq(details[0].to, address(0));
+        assertEq(details[0].requestedAmount, order.input.amount);
+        assertEq(details[1].to, address(this));
+        assertEq(details[1].requestedAmount, order.fee.startAmount);
     }
 }
