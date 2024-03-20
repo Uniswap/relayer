b94dd7f365860dafb87fd12e394996f4cf522376
Sara Reynolds <30504811+snreynolds@users.noreply.github.com>
2024-03-20 13:05:50 -0400

    save initcode and hash (#100)
    * save initcode and hash
    
    * change pragma

---
+++ script/RelayOrderReactorInitBytecode.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.24;
+
+import "forge-std/console2.sol";
+import "forge-std/Script.sol";
+import {RelayOrderReactor} from "../src/reactors/RelayOrderReactor.sol";
+
+contract RelayOrderReactorInitBytecode is Script {
+    function setUp() public {}
+
+    function run(address universalRouter) public {
+        bytes memory args = abi.encode(universalRouter);
+        bytes memory initcode = abi.encodePacked(vm.getCode("RelayOrderReactor.sol:RelayOrderReactor"), args);
+
+        bytes32 initcodeHash = keccak256(initcode);
+
+        vm.writeFile(".artifacts/init_code", vm.toString(initcode));
+        vm.writeFile(".artifacts/init_code_hash", vm.toString(initcodeHash));
+
+        console2.logBytes32(initcodeHash);
+    }
+}
